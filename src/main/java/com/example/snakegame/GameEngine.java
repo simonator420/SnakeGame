550bb58c04bc811bc735bfbcedd8c4b29a5dc4ce
xsalaj <xsalaj@mendelu.cz>
2023-10-03 22:05:09 +0200

    Checkerboard background

--- src/main/java/com/example/snakegame/GameEngine.java
+++ src/main/java/com/example/snakegame/GameEngine.java
@@ -2,13 +2,17 @@ package com.example.snakegame;
 
 import javafx.application.Platform;
 import javafx.scene.Scene;
+import javafx.scene.canvas.Canvas;
+import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.layout.StackPane;
 import javafx.scene.layout.VBox;
 import javafx.scene.paint.Color;
+import javafx.scene.paint.Stop;
 import javafx.stage.Stage;
 import javafx.scene.text.Font;
+import javafx.scene.canvas.GraphicsContext;
 
 public class GameEngine {
 
@@ -17,26 +21,39 @@ public class GameEngine {
     private Font gameNameFont = Font.loadFont(getClass().getResourceAsStream("/com/example/snakegame/snake_font.otf"), 90);
     private Font gameAlternativeFont = Font.loadFont(getClass().getResourceAsStream("/com/example/snakegame/snake_font_alternative.otf"), 90);
     private static final int WIDTH = 700;
-    private static final int HEIGHT = 700;
+    private static final int HEIGHT = WIDTH;
+    private static final int ROWS = 20;
+    private static final int COLUMNS = ROWS;
+    private static final int SQUARE_SIZE = WIDTH/COLUMNS;
+    private GraphicsContext gc;
 
     public void startGame(Stage primaryStage){
+        // Drawing the background
+        Canvas backgroundCanvas = new Canvas(WIDTH, HEIGHT);
+        GraphicsContext backgroundGC = backgroundCanvas.getGraphicsContext2D();
+        drawCheckerboard(backgroundGC);
+
+        // Game name
         Label gameName = new Label("Ultimate Snake");
         gameName.setFont(gameAlternativeFont);
         gameName.setTextFill(Color.RED);
         gameName.setTranslateY(-60);
 
+        // Start button
         Button startButton = new Button("START");
         startButton.setOnAction(e -> primaryStage.setScene(mainScene));
         startButton.setStyle("-fx-background-color: RED; -fx-text-fill: white; -fx-font-size: 18px;");
         startButton.setTranslateY(30);
 
+        // Exit button
         Button exitButton = new Button("EXIT");
         exitButton.setOnAction(e -> Platform.exit());
         exitButton.setStyle("-fx-background-color: RED; -fx-text-fill: white; -fx-font-size: 18px;");
         exitButton.setTranslateY(80);
 
+        // Adding elements from start menu
         StackPane menuLayout = new StackPane();
-        menuLayout.getChildren().addAll(gameName, startButton, exitButton);
+        menuLayout.getChildren().addAll(backgroundCanvas,gameName, startButton, exitButton);
         menuScene = new Scene(menuLayout, WIDTH, HEIGHT);
 
         // Main stage settings
@@ -50,6 +67,22 @@ public class GameEngine {
         primaryStage.setScene(menuScene);
         primaryStage.setTitle("Ultimate Snake");
         primaryStage.show();
+    }
+
+    // Creating the checkerboard background
+    private void drawCheckerboard(GraphicsContext gc) {
+        for (int row = 0; row < ROWS; row++) {
+            for (int col = 0; col < COLUMNS; col++) {
+                if ((row + col) % 2 == 0) {
+                    gc.setFill(Color.WHITE);
+                } else {
+                    gc.setFill(Color.web("#f2f2f2"));
+                }
+                double x = col * SQUARE_SIZE;
+                double y = row * SQUARE_SIZE;
+                gc.fillRect(x, y, SQUARE_SIZE, SQUARE_SIZE);
+            }
+        }
     }
 
 }
